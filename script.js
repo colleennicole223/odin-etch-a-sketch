5e44ef0a434e39629bd05858ec07cc8725e98c5a
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-07-24 17:52:56 -0400

    Updated JS to be able to draw on canvas using event listeners

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 
 const grid = document.querySelector("#gridBlock");
-setGrid(64);
 
+setGrid(24);
 
 
 function setGrid(value){
     i = 0;
     // k = (600-((1)*(value-1)))/value;
     k = 600/value;
+    var isClicked = false;
+    grid.draggable = false;
     while(i<(value * value)){
         const square = document.createElement("div");
-        square.classList.add("square");
-        // square.textContent = i+1;
+        square.classList.add("square"+i);
         grid.appendChild(square);
-        console.log("Hello"+(2^2)+" "+k);
         i++;
         square.style.width=k+"px";
         square.style.height=k+"px";
-        console.log(gridBlock.style.gap);
+        square.style.backgroundColor = "white"
+        square.draggable = false;
+        // square.addEventListener("click", () => {
+        //     square.style.backgroundColor = "black"
+        // });
+
+        // square.addEventListener("mouseover", () => {
+        //     square.style.backgroundColor = "black"
+        // });
+        square.addEventListener(("mousedown"), () => {
+            isClicked = true;
+            
+        });
+
+        square.addEventListener(("mouseover"), () => {
+            if(isClicked==true){
+                square.style.backgroundColor = "black";
+            }
+        });
+
+        square.addEventListener(("mouseup"), () => {
+            isClicked = false;
+        });
+
+        grid.addEventListener("mouseleave", () => {
+            isClicked = false;
+        });
+
     }
 }
+
 
