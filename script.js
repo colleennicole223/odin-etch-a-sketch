233e860d5f97d7a2c72030eb5712c17548c0b011
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-07-30 14:52:26 -0400

    Updated event listeners to respond to radio input of Drawing Method

--- script.js
+++ script.js
@@ -2,32 +2,22 @@
 const grid = document.querySelector("#gridBlock");
 const color = document.querySelector("#colorSelection")
 const gridSize = document.querySelector("#gridSize")
+const drawSelection = document.querySelectorAll('#drawingMethod');
+
+// Initilize variables 📃
+let draw = "clickDrag"
+let isClicked = false;
 
 // Create the grid of squares 🔮
-updateGrid(16,color.value);
+updateGrid(16, color.value, draw);
 
-// select all of the squares 
+// select all of the newly created squares 
 let squares = document.querySelectorAll('square');
 
 // when the color picked is changed, update the listeners 🔊 for each square with new color 🎨
 color.addEventListener("input", () =>{
-    var isClicked = false;
     squares.forEach(square => {
-        square.addEventListener(("mousedown"), () => {
-            isClicked = true;
-            square.style.backgroundColor = color.value;
-        });
-        square.addEventListener(("mouseover"), () => {
-            if(isClicked==true){
-                square.style.backgroundColor = color.value;
-            }
-        });
-        square.addEventListener(("mouseup"), () => {
-            isClicked = false;
-        });
-        grid.addEventListener("mouseleave", () => {
-            isClicked = false;
-        });
+        updateListener(square, color.value, draw);
     });
 })
 
@@ -41,15 +31,26 @@ gridSize.addEventListener("change", () =>{
     squares = document.querySelectorAll('square');
 })
 
+drawSelection.forEach(selection => {
+    selection.addEventListener("click", () => {
+        isClicked = false;
+        squares.forEach(square => {
+            updateListener(square, color.value, selection.value);
+        });
+        console.log("Drawing method updated, draw = "+selection.value);
+    })
+})
+
+
 // Functions ------------------------------------------------------------------------
 
 
 // Grid creation and inital square color listeners 🔊
-function updateGrid(value,color){
+function updateGrid(value, color, draw){
     i = 0;
     // k = (600-((1)*(value-1)))/value;
     k = 600/value;
-    var isClicked = false;
+    
     grid.draggable = false;
 
     while(i<(value * value)){
@@ -61,33 +62,41 @@ function updateGrid(value,color){
         square.style.height=k+"px";
         square.style.backgroundColor = "white"
         square.draggable = false;
-        // square.addEventListener("click", () => {
-        //     square.style.backgroundColor = "black"
-        // });
-
-        // square.addEventListener("mouseover", () => {
-        //     square.style.backgroundColor = "black"
-        // });
-
-        square.addEventListener(("mousedown"), () => {
-            isClicked = true;
-            square.style.backgroundColor = color;
-        });
-        square.addEventListener(("mouseover"), () => {
-            if(isClicked==true){
-                square.style.backgroundColor = color;
-            }
-        });
-        square.addEventListener(("mouseup"), () => {
-            isClicked = false;
-        });
-        grid.addEventListener("mouseleave", () => {
-            isClicked = false;
-        });
-
+        updateListener(square, color, draw);
     }
-    
 }
 
+function updateListener(square, color, drawInput){
 
+    draw = drawInput;
+    
+    square.addEventListener(("mousedown"), () => {
+        if(draw == "clickDrag"){
+            isClicked = true;
+            square.style.backgroundColor = color;
+        }
+    });
 
+    square.addEventListener(("mouseover"), () => {
+        console.log(isClicked)
+        if(isClicked == true){
+            square.style.backgroundColor = color;
+            console.log("hovering while clicked is true");
+        }else if(isClicked == false && draw == "hover"){
+            square.style.backgroundColor = color;
+            console.log("hovering while clicked is false but hover is true");
+        }
+    });
+
+    square.addEventListener(("mouseup"), () => {
+        if(draw == "clickDrag"){
+            isClicked = false;
+        }
+    });
+
+    grid.addEventListener("mouseleave", () => {
+        if(draw == "clickDrag"){
+            isClicked = false;
+        }
+    });
+}
